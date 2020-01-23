2075f5f5c0a7a267c6871b5e8dc8608fc54b8058
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-23 11:42:47 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,25 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill
 
 class TomatoSkill(MycroftSkill):
     def __init__(self):
-        super().__init__()
+        MycroftSkill.__init__(self)
 
-    @intent_handler('what.is.a.tomato.intent')
+    def initialize(self):
+        self.register_intent_file('what.is.intent', self.handle_what_is)
+        self.register_intent_file('do.you.like.intent', self.handle_do_you_like)
+
     def handle_what_is(self, message):
-        self.speak_dialog('tomato.description')
+        self.speak('A tomato is a big red thing')
 
-    @intent_handler('do.you.like.intent')
     def handle_do_you_like(self, message):
         tomato_type = message.data.get('type')
         if tomato_type is not None:
-            self.speak_dialog('like.tomato.type',{'type': tomato_type})
+            self.speak("Well, I'm not sure if I like " + tomato_type + " tomatoes.")
         else:
-            self.speak_dialog('like.tomato.generic')
-
+            self.speak('Of course I like tomatoes!')
+     def handler(self, message):
+        word = message.data.get('your_keyword') # if not present will return None
+     
     def stop(self):
         pass
 
