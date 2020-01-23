967c63270d2ee93b6686dabeae699703a0489bde
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-23 15:59:15 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,18 +5,18 @@ class TomatoSkill(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        self.register_intent_file('what.is.intent', self.handle_what_is)
-        self.register_intent_file('do.you.like.intent', self.handle_do_you_like)
+        self.register_intent_file('what.is.intent', self.handle_what_is) #register the intentes
+        self.register_intent_file('do.you.like.intent', self.handle_do_you_like) #register the intentes
 
-    def handle_what_is(self, message):
-        self.speak('A tomato is a big red thing')
+    def handle_what_is(self, message): #get the intent
+        self.speak('A tomato is a big red thing') #
 
     def handle_do_you_like(self, message):
-        tomato_type = message.data.get('type')
+        tomato_type = message.data.get('type')  #get the specific keword into type
         if tomato_type is not None:
-            self.speak("Well, your specific keyword is " + tomato_type + " in your word.")
+            self.speak("Well, your specific keyword is " + tomato_type + " in your word.") #print specific keword
         else:
-            self.speak('Of course I like tomatoes!')
+            self.speak('you are not given any specific word!') #print if specific keword is not given
 
 
     def stop(self):
